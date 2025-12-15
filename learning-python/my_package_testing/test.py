3b47648f45714d6439db0311026b8e47239d784d
rohitagarwal-gkmit <rohitagarwal@gkmit.co>
2025-12-15 14:16:06 +0530

    feat: add greeting message using say_hello in test.py

--- learning-python/my_package_testing/test.py
+++ learning-python/my_package_testing/test.py
@@ -1,6 +1,9 @@
 from rohit_math_helper import MathHelper
+from nax_hello import say_hello
 
 if __name__ == "__main__":
+    print(say_hello.print_hello("Rohit"))
+
     print("Addition:", MathHelper.add(10, 5))
     print("Subtraction:", MathHelper.subtract(10, 5))
     print("Multiplication:", MathHelper.multiply(10, 5))
